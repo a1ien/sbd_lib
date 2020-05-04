ed18c03ed1ce877d7f66bce8579ab6c2c0f8ca2b
Ilya Averyanov <averyanovin@gmail.com>
2020-05-04 18:27:00 +0300

    Remove failure dependencies

--- src/errors.rs
+++ src/errors.rs
@@ -1,52 +1,41 @@
-use failure::Fail;
 use std::convert::From;
 
-#[derive(Fail, Debug)]
+#[derive(Debug)]
 pub enum Error {
-    #[fail(display = "I/O error")]
-    Io(#[cause] ::std::io::Error),
+    /// I/O error
+    Io(::std::io::Error),
 
-    #[fail(display = "Invalid protocol revision number: {}", _0)]
+    /// Invalid protocol revision number
     InvalidProtocolRevisionNumber(u8),
 
     /// Invalid information element identifier.
-    #[fail(display = "Invalid information element identifier: {}", _0)]
     InvalidInformationElementIdentifier(u8),
 
     /// The timestamp is negative, but only positive ones are supported.
-    #[fail(display = "Negative timestamp: {}", _0)]
     NegativeTimestamp(i64),
 
     /// The overall message length is too long.
-    #[fail(display = "The overall message length is too long: {}", _0)]
     OverallMessageLength(usize),
 
     /// The payload is too long.
-    #[fail(display = "The payload is too long: {}", _0)]
     PayloadTooLong(usize),
 
     /// No headers in a message
-    #[fail(display = "NoHeaders headers in a message")]
     NoHeader,
 
     /// No payloads in an message.
-    #[fail(display = "Two payloads in a MO message")]
     NoPayload,
 
     /// Two headers in a message
-    #[fail(display = "Two headers in a message")]
     TwoHeaders,
 
     /// Two payloads in an message.
-    #[fail(display = "Two payloads in a MO message")]
     TwoPayloads,
 
     /// Two locations in an message.
-    #[fail(display = "Two locations in a MO message")]
     TwoLocations,
 
     /// The session status is unknown.
-    #[fail(display = "Unexpected EOF during reading {}.", _0)]
     UnknownSessionStatus(u8),
 }
 
